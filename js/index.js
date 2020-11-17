02b47b121b57549fc0b789358553cd7c06c77627
Daria <daria.ryskal@gmail.com>
2020-11-17 16:00:18 +0200

    Welcome block parallax

--- js/index.js
+++ js/index.js
@@ -51,8 +51,20 @@ $(document).ready(function () {
     }
   })
 
-  //Fixed header
+  //Parallax-viewport
+  function isInViewport(node) {
+    let rect = node.getBoundingClientRect()
+    return (
+      (rect.height > 0 || rect.width > 0) &&
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+  }
+
   $(window).on('scroll', function () {
+    //Fixed header
     let scroller = $(this).scrollTop()
     let header = $('.fixed')
     let fixedHeaderTop = $('.opportunity').offset().top
@@ -61,6 +73,21 @@ $(document).ready(function () {
       (scroller > fixedHeaderTop - 250) &&
       (scroller < fixedHeaderBottom - 400) ?
         'Down': 'Up')](300)
+
+    //Parallax
+    let scrolled = $(window).scrollTop()
+    $('.parallax').each(function(index, element) {
+      let initY = $(this).offset().top
+      let height = $(this).height()
+      let endY = initY + $(this).height()
+
+      let visible = isInViewport(this)
+      if(visible) {
+        let diff = scrolled - initY
+        let ratio = Math.round((diff / height) * 100)
+        $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px')
+      }
+    })
   })
 
   if ( $(window).width() > 992 ) {
