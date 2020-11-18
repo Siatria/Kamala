9feae629c66fa7a22e8df96dc463035fa4e9e33a
Daria <daria.ryskal@gmail.com>
2020-11-19 01:37:00 +0200

    some changes

--- js/index.js
+++ js/index.js
@@ -94,6 +94,25 @@ $(document).ready(function () {
     as++
   }
 
+  //Zoom off
+  document.addEventListener('mousewheel', function(e){
+    if(!e.ctrlKey && !e.metaKey) return
+    e.preventDefault()
+    e.stopImmediatePropagation()
+  }, {passive:false})
+
+  document.addEventListener('gesturestart', function(e){
+    e.preventDefault()
+    e.stopImmediatePropagation()
+  }, {passive:false})
+
+  document.addEventListener('keydown', function(e){
+    if(!e.ctrlKey && !e.metaKey) return
+    if(e.keyCode !== 189 && e.keyCode !== 187) return
+    e.preventDefault()
+    e.stopImmediatePropagation()
+  }, {passive:false})
+
   $(window).on('scroll', function () {
     //Fixed header
     let scroller = $(this).scrollTop()
