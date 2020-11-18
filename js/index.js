a89f5a25a18e507ceefdc066fb843b56d47bf2a6
Daria <daria.ryskal@gmail.com>
2020-11-18 23:47:18 +0200

    some changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-  //Font fix in Mozilla
-  if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
-    $('body').addClass('font-moz')
-  }
-  
   //Shows full form
   $('.continue-btn').on('click touchend', function (e) {
     e.preventDefault()
@@ -22,7 +17,7 @@ $(document).ready(function () {
   })
 
   //Smooth scroller on anchors
-  $('.header-register-btn, .register-btn').click(function () {
+  $('.header-register-btn, .register-btn, .logo').click(function () {
     let el = $(this).attr('href');
     $('html,body').animate({
       scrollTop: $(el).offset().top}, 1400);
@@ -33,24 +28,6 @@ $(document).ready(function () {
   if ( $(window).width() < 501 ) {
     $('.header-register-btn').text('Register')
   }
-
-  //Rotate select arrow
-  $('.select-amount').on('click', function (e) {
-    e.preventDefault()
-    $('.background-select').toggleClass('clicked')
-  })
-  $('.select-period').on('click', function (e) {
-    e.preventDefault()
-    $('.background-select2').toggleClass('clicked')
-  })
-
-  $(document.body).on('click', function(e){
-    if(!$(e.target).closest('.select-amount').length){
-      $('.background-select').removeClass('clicked')
-    } if(!$(e.target).closest('.select-period').length){
-      $('.background-select2').removeClass('clicked')
-    }
-  })
 
   //Slider
   $('.photo-slider').owlCarousel({
@@ -82,11 +59,51 @@ $(document).ready(function () {
     )
   }
 
+  //Stats
+  let as = 0
+  function number_to(id,from,to,duration) {
+    let element = document.getElementById(id)
+    let start = new Date().getTime()
+    setTimeout(function() {
+      let now = (new Date().getTime()) - start
+      let progress = now / duration
+      let result = Math.floor((to - from) * progress + from)
+      element.innerHTML = progress < 1 ? result : to
+      if (progress < 1) setTimeout(arguments.callee, 10)
+    }, 10)
+  }
+
+  function moveProgressBar() {
+    let progressBar = $('.progress-wrap')
+    let getPercent = (progressBar.data('progress-percent') / 100)
+    let getProgressWrapWidth = progressBar.width()
+    let progressTotal = getPercent * getProgressWrapWidth
+    let animationLength = 2000
+
+    $('.progress-bar').stop().animate({
+      left: progressTotal
+    }, animationLength)
+  }
+
+  moveProgressBar()
+  if (as === 0) {
+    number_to('investors',0,45,2000);
+    number_to('days',0,30,2000);
+    number_to('yield',0,9,2000);
+    number_to('funded',0,125000,2000);
+    as++
+  }
+
   $(window).on('scroll', function () {
     //Fixed header
     let scroller = $(this).scrollTop()
-    let header = $('.fixed')
-    let scr = header['slide' + ((scroller > 80) ? 'Down': 'Up')](300)
+    let headerBtn = $('.header-register-btn')
+    let fixedHeaderTop = $('.slider').offset().top
+    let fixedHeaderBottom = $('.form-wrap').offset().top
+    let scr = headerBtn['fade' + (
+      (scroller > fixedHeaderTop - 100) &&
+      (scroller < fixedHeaderBottom - 400) ?
+        'In': 'Out')](300)
 
     //Parallax
     let scrolled = $(window).scrollTop()
@@ -103,46 +120,4 @@ $(document).ready(function () {
       }
     })
   })
-
-  if ( $(window).width() > 992 ) {
-    let as = 0
-    $(window).on('scroll', function () {
-      function number_to(id,from,to,duration) {
-        let element = document.getElementById(id)
-        let start = new Date().getTime()
-        setTimeout(function() {
-          let now = (new Date().getTime()) - start
-          let progress = now / duration
-          let result = Math.floor((to - from) * progress + from)
-          element.innerHTML = progress < 1 ? result : to
-          if (progress < 1) setTimeout(arguments.callee, 10)
-        }, 10)
-      }
-
-      function moveProgressBar() {
-        let progressBar = $('.progress-wrap')
-        let getPercent = (progressBar.data('progress-percent') / 100)
-        let getProgressWrapWidth = progressBar.width()
-        let progressTotal = getPercent * getProgressWrapWidth
-        let animationLength = 2000
-
-        $('.progress-bar').stop().animate({
-          left: progressTotal
-        }, animationLength)
-      }
-
-      let progress = $('.stats-wrap').offset().top
-
-      if ($(this).scrollTop() > progress - 500) {
-        moveProgressBar()
-        if (as === 0) {
-          number_to('investors',0,45,2000);
-          number_to('days',0,30,2000);
-          number_to('yield',0,9,2000);
-          number_to('funded',0,125000,2000);
-          as++
-        }
-      }
-    })
-  }
 })
